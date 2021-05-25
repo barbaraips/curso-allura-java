346a76deea46eb1eca9368a3d18320a8bd7e2892
Barbara <Barbara.Santos-ext@ab-inbev.com>
2021-05-25 18:08:45 -0300

    Dates excercises

--- src/main/java/Cursos.java
+++ src/main/java/Cursos.java
@@ -6,12 +6,12 @@ import java.util.OptionalDouble;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-class Curso {
+class Cursos {
     
     private final String nome;
     private final int alunos;
     
-    public Curso(final String nome, final int alunos) {
+    public Cursos(final String nome, final int alunos) {
         
         this.nome = nome;
         this.alunos = alunos;
@@ -19,31 +19,31 @@ class Curso {
     
     public static void main(final String[] args) {
         
-        final List<Curso> cursos = new ArrayList<>();
-        cursos.add(new Curso("Python", 45));
-        cursos.add(new Curso("JavaScript", 150));
-        cursos.add(new Curso("Java 8", 113));
-        cursos.add(new Curso("C", 55));
+        final List<Cursos> cursos = new ArrayList<>();
+        cursos.add(new Cursos("Python", 45));
+        cursos.add(new Cursos("JavaScript", 150));
+        cursos.add(new Cursos("Java 8", 113));
+        cursos.add(new Cursos("C", 55));
         
-        cursos.sort(Comparator.comparing(Curso::getAlunos));
+        cursos.sort(Comparator.comparing(Cursos::getAlunos));
         
         //cursos com mais de 50 alunos
         cursos.stream().filter(curso -> curso.getAlunos() > 50).forEach(curso -> System.out.println(curso.getNome()));
-        final Stream<String> nomes = cursos.stream().map(Curso::getNome);
+        final Stream<String> nomes = cursos.stream().map(Cursos::getNome);
         
         //quantidade de alunos nos cursos com mais de 50 alunos
-        cursos.stream().filter(c -> c.getAlunos() > 50).map(Curso::getAlunos).forEach(System.out::println);
+        cursos.stream().filter(c -> c.getAlunos() > 50).map(Cursos::getAlunos).forEach(System.out::println);
         
         //primeiro curso com mais de 50 alunos
-        final Optional<Curso> first = cursos.stream().filter(c -> c.getAlunos() > 50).findFirst();
+        final Optional<Cursos> first = cursos.stream().filter(c -> c.getAlunos() > 50).findFirst();
         System.out.println(first.get().getNome());
         
         //quantidade média de alunos de todos os cursos
-        final OptionalDouble average = cursos.stream().mapToInt(Curso::getAlunos).average();
+        final OptionalDouble average = cursos.stream().mapToInt(Cursos::getAlunos).average();
         System.out.println("Media de alunos: " + average.getAsDouble());
         
         //transformar stream em lista
-        final List<Curso> cursosFiltrados = cursos.stream().filter(c -> c.getAlunos() > 50).collect(Collectors.toList());
+        final List<Cursos> cursosFiltrados = cursos.stream().filter(c -> c.getAlunos() > 50).collect(Collectors.toList());
     }
     
     public String getNome() {
---
+++ src/main/java/Dates.java
@@ -0,0 +1,27 @@
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+
+public class Dates {
+    
+    public static void main(final String[] args) {
+        
+        //Hoje
+        final LocalDate agora = LocalDate.now();
+        System.out.println(agora);
+        
+        // 25 de Janeiro de 2099
+        final LocalDate futuro = LocalDate.of(2099, 1, 25);
+        System.out.println(futuro);
+        
+        //Intervalo:
+        final Period intervalo = Period.between(agora, futuro);
+        System.out.println(intervalo);
+        
+        //Data formatada  dd/MM/yyyy
+        final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        final String dataFormatada = formatter.format(agora);
+        System.out.println(dataFormatada);
+    }
+    
+}
